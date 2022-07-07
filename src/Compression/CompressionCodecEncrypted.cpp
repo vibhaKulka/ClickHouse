0e1da08c11d3a7aca5d855610bcb289d3bb2d437
vibhaKulka <vibha.kulkarni@in.ibm.com>
2022-07-07 13:57:36 +0000

    support for z(s390x)

--- src/Compression/CompressionCodecEncrypted.cpp
+++ src/Compression/CompressionCodecEncrypted.cpp
@@ -9,6 +9,8 @@
 #include <Common/logger_useful.h>
 
 // This depends on BoringSSL-specific API, notably <openssl/aead.h>.
+#undef USE_SSL
+#define USE_SSL 0
 #if USE_SSL
 #include <openssl/digest.h>
 #include <openssl/err.h>
